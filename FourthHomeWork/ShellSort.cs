d4677d9c52535115711284ec0c1093dc520c7f56
AlexanderZnak <frolovruslan1990@gmail.com>
2021-11-10 22:48:35 +0300

    Arrays and Collections

---
+++ FourthHomeWork/ShellSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FourthHomeWork
+{
+    public class ShellSort : ISorting
+    {
+        void ISorting.Sort()
+        {
+            int[] number = {23, 65, 7, 789, -45, 34, 8, -12, -67, 43};
+            int j;
+            int step = number.Length / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < (number.Length - step); i++)
+                {
+                    j = i;
+                    while ((j >= 0) && (number[j] > number[j + step]))
+                    {
+                        int temp = number[j];
+                        number[j] = number[j + step];
+                        number[j + step] = temp;
+                        j -= step;
+                    }
+                }
+
+                step = step / 2;
+            }
+
+            Array.Reverse(number);
+            foreach (var num in number)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
